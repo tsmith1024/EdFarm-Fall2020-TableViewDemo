9c42ebcb255d3c0ad451350dc150c7f3cfc41a9f
Taylor Smith <tsmith1024@gmail.com>
2020-11-05 17:59:11 -0600

    Initial Commit

---
+++ TableViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Taylor Smith on 11/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
